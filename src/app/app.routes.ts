e61b66d0c248edc80507076bb72443f7f6de4c64
Panshuffle <francisco.llanos@alumnos.ucn.cl>
2024-12-27 04:43:19 -0300

    Se añadió la base del menú principal.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch:"full"},  // Redirige a login por defecto
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'main',
+    component: MainComponent
   },
 ];
